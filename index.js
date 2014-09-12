6bc7f3084428abf55f7f71108e110e2c64e3a905
Sun Kim <sunkim924@gmail.com>
2014-09-12 00:04:09 -0400

    added redis

--- index.js
+++ index.js
@@ -1,20 +1,51 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var redis = require('redis');
+var redisClient = redis.createClient();
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname+'/index.html');
+var messages = [];
+
+app.get('/', function(req,res){
+  res.sendFile(__dirname+'/views/home.html');
 });
+
+app.get('/chat', function(req, res){
+  res.sendFile(__dirname+'/views/index.html');
+});
+
+var storeMessage = function(name, data){
+  var message = JSON.stringify({name: name, data: data});
+  // only keep last 20 messages
+  redisClient.lpush('messages', message, function(err, res){
+    redisClient.ltrim('messages', 0, 20);
+  })
+}
+
 
 // listen on connection event for incoming sockets
 io.on('connection', function(socket){
   socket.on('join', function(username){
     // set username associated with a client
     socket.username = username;
+    io.emit('join', username + ' has joined.');
+
+    redisClient.lrange('messages', 0, -1, function(err, message){
+      messages = messages.reverse();
+      messages.forEach(function(message){
+        message = JSON.parse(message);
+        io.emit('messages', message.name + ": " + message.data);
+      })
+    });
+
+
   });
   // on incoming 'chat message' event, send message
   socket.on('chat message', function(msg){
     // print username of client with broadcasting message
+    socket.on(socket.username, function(err, name){
+      storeMessage(name, msg);
+    })
     io.emit('chat message', socket.username + ': ' + msg);
   });
 });
